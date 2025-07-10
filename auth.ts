1d22a2b41ce454471756302dbd7557ba02e3dedd
takahiro-okada <okahanako09875@gmail.com>
2025-07-11 08:07:33 +0900

    🐛: Refactor customer fetching logic and improve error handling; update login form with default values; add bcrypt dependency

--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { authConfig } from "./auth.config";
-import { z } from "zod";
-import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
 import postgres from "postgres";
+import { z } from "zod";
+import type { User } from "@/app/lib/definitions";
+import { authConfig } from "./auth.config";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -29,10 +29,15 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
+
           const user = await getUser(email);
           if (!user) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
         }
 
+        console.log("Invalid credentials");
         return null;
       },
     }),
